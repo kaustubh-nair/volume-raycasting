8fbddbb8d78b10fc932980a9ccf86bc7af154afa
Kaustubh Nair <kaustubh.nair108@gmail.com>
2021-11-16 07:46:04 +0530

    Remove hsv TFS from UI

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -52,19 +52,7 @@ private slots:
 
     void on_loadVolume_clicked();
 
-    void on_tf_slider_valueChanged(int value);
-
     void on_background_clicked();
-
-    void on_tf_checkbox_clicked(bool value);
-
-    void on_HSV_TF_h_slider_valueChanged(int value);
-
-    void on_HSV_TF_s_slider_valueChanged(int value);
-
-    void on_HSV_TF_v_slider_valueChanged(int value);
-
-    void on_hsv_tf_checkbox_clicked(bool value);
 
     void on_height_spinbox_valueChanged();
 
